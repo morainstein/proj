e1e9510435dcde8e3ee9713da7a16d81a99f0595
morainstein <nichroberto@gmail.com>
2025-06-16 20:37:25 -0300

    Initial commit

---
+++ error.php
@@ -0,0 +1,20 @@
+<?php
+
+var_dump($_GET);
+
+define("DB_NAME",'php');
+define("DB_USER",'root');
+define("DB_PASS",'123');
+
+try{
+  $pdo = new PDO('mysql:host=localhost;dbname=php','root','123');
+}catch(PDOException $e){
+  echo $e->getMessage();
+  echo '<br>';
+  echo $e->getCode();
+  echo '<br>';
+  echo $e->getTraceAsString();
+}
+
+echo '<br>';
+echo 'FINAL';
---
+++ git.md
@@ -0,0 +1,7 @@
+# GIT / GitHub
+- O que é
+- Iniciar um repositório
+- Versionamento
+  - Envio (commit e pull)
+  - Busca (pull )
+- Branchs
